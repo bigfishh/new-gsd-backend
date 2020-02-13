56ac8e0b18f06be79250ef83af60ffb16e815b32
bigfishh <anniezheng.anniee@gmail.com>
2020-02-13 14:09:07 -0500

    final draft

--- app/controllers/todo_tasks_controller.rb
+++ app/controllers/todo_tasks_controller.rb
@@ -1,4 +1,6 @@
 class TodoTasksController < ApplicationController
+    # before_action :authorized, only: [:index, :show]
+
     def index
         @todo_tasks = TodoTask.all
         # @todoObj = TodoTask.all
@@ -11,11 +13,13 @@ class TodoTasksController < ApplicationController
       end
     
       def create
+        byebug
         @todo_task = TodoTask.create(todo_task_params)
+        render json: @todo_task
       end
     
       private
       def todo_task_params
-        params.permit(:todoDate, :title, :description, :completed, :category, :user)
+        params.permit(:todoDate, :title, :description, :completed, :category_id, :user_id)
       end
 end
